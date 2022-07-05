4cd193fac64af79c90ab84c6346d03e3c1a5faea
DOTL-Amir-R <raeisi.amirrr@gmail.com>
2022-07-05 13:25:18 +0430

    HWJS01V01

---
+++ index.js
@@ -0,0 +1,54 @@
+console.log("Hi,This is six data type in javascript")
+// these are some data types in javascript
+let first="amir";
+console.log(typeof(first));
+let second ;
+console.log(typeof(second));
+let third=1;
+console.log(typeof(third));
+// this is an array type but arraies are consider objects in javascript and we have some other data types in javascript that considered as objects
+let arrayOne=["amir","ali"];
+console.log(typeof(arrayOne));
+const x=1 ; const y=2;
+console.log(typeof(x>y));
+console.log(null);
+// now I want to show you some math works in the javascript
+console.log("these are some math functions in the javascript");
+
+console.log(Math.ceil(5, 0));
+console.log(Math.floor(5, 0));
+console.log(Math.max(5, 0));
+console.log(Math.min(5, 0));
+
+
+// here I will change some strings to a number data type
+let amir=Number("amir");
+console.log(typeof(amir));
+let ali=parseInt("ali");
+console.log(typeof(ali));
+console.log("this" + 5);
+console.log(typeof("this" + 5));
+// here at the end im using some if functions that are in javascript
+// let example01=5
+let example02=3
+if (example01>5){
+    console.log("correct");
+}
+else if(example01=5){
+    console.log("its equal to 5");
+}
+else{
+console.log("incorrect");
+}
+
+switch(example02){
+    case example02=1:
+        console.log("its less than one");
+        break;
+    case example02=2:
+        console.log("its equal to 2");
+        break;
+    case example02=3:
+        console.log("its equal to 3");
+        break;
+}
